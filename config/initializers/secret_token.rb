a268e0166769964bde0e3eed509370968a343908
cookieroz <cookieroz@gmail.com>
2012-09-26 06:52:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrandesVillas::Application.config.secret_token = 'f1808e942a0783752fb0b4b7eaeac863a540b91975ab01caa59ca2825d202ab3c1e2c72462aeff846a5a32d15e6f391ea37574f2689439478696fa5c7f4e05df'
