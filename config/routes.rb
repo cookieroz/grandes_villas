8101d859696884f957f7a78cd08be9fb838d8f7d
cookieroz <cookieroz@gmail.com>
2012-10-29 13:33:20 -0700

    started changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
 GrandesVillas::Application.routes.draw do
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :costa_tropicals
 
-  resources :costa_tropicals
+    match '/help',    to: 'static_pages#help'
+    match '/about',   to: 'static_pages#about'
+    match '/contact', to: 'messages#new'
+    match '/owner', to: 'static_pages#owner'
 
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'messages#new'
-  match '/owner', to: 'static_pages#owner'
+    resources :villas do
+      resources :comments
+    end
 
-  resources :villas do
-    resources :comments
+    resources :messages
+
+    get "photos/index"
+
+    get "photos/show"
+
+    get "home/index"
+
+    root :to => 'home#index'
   end
-
-  resources :messages
-
-  get "photos/index"
-
-  get "photos/show"
-
-  get "home/index"
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  match '', to: redirect("/#{I18n.default_locale}")
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -68,7 +73,6 @@ GrandesVillas::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
