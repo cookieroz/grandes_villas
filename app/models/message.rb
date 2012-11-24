18971d9bcc3cb625555e9d6064a8d6d4e36d3545
cookieroz <cookieroz@gmail.com>
2012-11-24 05:07:53 -0800

    changed villa form styles

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,10 @@ class Message
   attribute :name
   attribute :email
   attribute :content
+  attribute :tlf
+  attribute :dates
+  attribute :adults
+  attribute :kids
 
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
