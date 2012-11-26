5c37162412e1f7a9ff2742d551f5786bd5d0c58b
cookieroz <cookieroz@gmail.com>
2012-11-26 03:01:57 -0800

    added services page and filter for homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,16 @@ class HomeController < ApplicationController
   def index
     @villas = Villa.scoped.limit(4).order("created_at DESC")
     @home_page = true
+
+    if params[:location_id]
+      @locations = Location.all
+      @villas = Villa.by_locations(params[:location_id])
+    elsif params[:sleeps]
+      @villas = Villa.find_all_by_sleeps(params[:sleeps][:num])
+    elsif params[:bedrooms]
+      @villas = Villa.find_all_by_bedrooms(params[:bedrooms][:num])
+    else
+      @villas = Villa.all
+    end
   end
 end
