10f9479df5185c065c5c60d2827b9ef9e81d3362
cookieroz <cookieroz@gmail.com>
2013-01-05 01:12:28 -0800

    made changes to contact form to send inquiry

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,7 @@ class MessagesController < ApplicationController
     @message = Message.new(params[:message])
     if @message.valid?
       ContactMailer.contact_confirmation(@message).deliver
+      ContactMailer.inquiry(@message).deliver
       redirect_to root_url, notice: "Message sent! Thank you for contacting us."
     else
       render "new"
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,9 +6,33 @@ class ContactMailer < ActionMailer::Base
   #
   #   en.contact_mailer.contact_confirmation.subject
   #
+  #def receive(email)
+  #  page = Page.find_by_address(email.to.first)
+  #  page.emails.create(
+  #      :subject => email.subject,
+  #      :body => email.body
+  #  )
+
+    #if email.has_attachments?
+    #  email.attachments.each do |attachment|
+    #    page.attachments.create({
+    #                                :file => attachment,
+    #                                :description => email.subject
+    #                            })
+    #  end
+    #end
+ # end
+
+
   def contact_confirmation(message)
     @message = message
 
     mail to: message.email, subject: "Grandes Villas Request"
+  end
+
+  def inquiry(message)
+    @message = message
+
+    mail to: "cookieroz@gmail.com", subject: "Grandes Villas Request"
   end
 end
